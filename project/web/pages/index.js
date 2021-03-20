b43a218244c4e511dad740993a2d8caf519b1e97
devheptagon <dev@heptagonsoft.com>
2021-03-20 23:10:20 +0000

    design 2

--- project/web/pages/index.js
+++ project/web/pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/home.module.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 export default function Home() {
   return (
@@ -72,10 +74,37 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div style={{ backgroundColor: "red" }}>
-          asdf
-          <br />
-          asdf
+        <div className="container-fluid">
+          <div className={styles.slide}>
+            <h1>Discover Our Featured Listings </h1>
+            <h2>Our featured suggestions can save you time</h2>
+            <Carousel className={styles.carousel} showArrows={true} swipeable>
+              <div>
+                <img src="./1.jpg" />
+                <p className="legend">
+                  Pizza Restaurant
+                  <br />
+                  £180.000
+                </p>
+              </div>
+              <div>
+                <img src="./2.jpg" />
+                <p className="legend">
+                  Fast food Restaurant
+                  <br />
+                  £120.000
+                </p>
+              </div>
+              <div>
+                <img src="./3.jpg" />
+                <p className="legend">
+                  Pizza Restaurant
+                  <br />
+                  £390.000
+                </p>
+              </div>
+            </Carousel>
+          </div>
         </div>
       </div>
       <footer className="container-fluid">
