2ec74a63fb82499f330369221d176a06ee445636
devheptagon <dev@heptagonsoft.com>
2021-03-20 21:44:25 +0000

    design 1

--- project/web/pages/_app.js
+++ project/web/pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+import "../styles/font-awesome-4.7.0/css/font-awesome.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- project/web/pages/index.js
+++ project/web/pages/index.js
@@ -1,65 +1,52 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+    <div className={styles.app}>
+      <nav className="navbar navbar-light bg-light" aria-label="social media">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src="./logo.png" alt="logo" />
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.contact}>
+            <ul>
+              <li>
+                <i className="fa fa-phone" aria-hidden="true"></i>
+              </li>
+              <li>
+                <div>01633-216899</div>
+                <div>info@linxbiz.com</div>
+              </li>
+              <li>
+                <i className="fa fa-facebook" aria-hidden="true"></i>
+              </li>
+              <li>
+                <i className="fa fa-twitter" aria-hidden="true"></i>
+              </li>
+              <li>
+                <i className="fa fa-linkedin" aria-hidden="true"></i>
+              </li>
+              <li>
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+              </li>
+              <li>
+                <i className="fa fa-youtube-play" aria-hidden="true"></i>
+              </li>
+            </ul>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </nav>
+      <nav className="container-fluid">
+        <div className="container">
+          <ul>
+            <li>HOME</li>
+            <li>BUSINESS</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
