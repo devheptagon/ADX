b73d394c937c567997ab4fb6279a6b9c455c3ea3
devheptagon <dev@heptagonsoft.com>
2021-03-25 22:42:50 +0000

    f

--- project/sanity/deskStructure.js
+++ project/sanity/deskStructure.js
@@ -1,30 +1,14 @@
 import React from "react";
 import S from "@sanity/desk-tool/structure-builder";
+import {
+  getDocumentsMenu,
+  getSettingsMenu,
+  hideTopMenu,
+} from "./helpers/appHelper";
+
+hideTopMenu();
 
 export default () =>
   S.list()
-    .title("Base")
-    .items([
-      S.listItem()
-        .title("Settings")
-        .child(
-          S.list()
-            .title("Settings")
-            .items([
-              ...S.documentTypeListItems().filter((item) =>
-                ["tag", "sector"].includes(item.getSchemaType().name)
-              ),
-            ])
-        ),
-      ...S.documentTypeListItems().filter(
-        (item) => !["tag", "sector"].includes(item.getSchemaType().name)
-      ),
-    ]);
-
-const timer = setInterval(() => {
-  const element = document.querySelector("div[class*='Navbar_root']");
-  if (element) {
-    element.style.display = "none";
-    clearInterval(timer);
-  }
-}, 5000);
+    .title("Menu")
+    .items([getSettingsMenu(), ...getDocumentsMenu()]);
---
+++ project/sanity/helpers/appHelper.js
@@ -0,0 +1,34 @@
+import React from "react";
+import S from "@sanity/desk-tool/structure-builder";
+
+const settingsMenuItems = ["tag", "sector"];
+
+export const hideTopMenu = () => {
+  const timer = setInterval(() => {
+    const element = document.querySelector("div[class*='Navbar_root']");
+    if (element) {
+      element.style.display = "none";
+      clearInterval(timer);
+    }
+  }, 5000);
+};
+
+export const getSettingsMenu = () => {
+  return S.listItem()
+    .title("Settings")
+    .child(
+      S.list()
+        .title("Settings")
+        .items([
+          ...S.documentTypeListItems().filter((item) =>
+            settingsMenuItems.includes(item.getSchemaType().name)
+          ),
+        ])
+    );
+};
+
+export const getDocumentsMenu = () => {
+  return S.documentTypeListItems().filter(
+    (item) => !settingsMenuItems.includes(item.getSchemaType().name)
+  );
+};
