253923839dbc5dadc49da56bddc361bc6f5e1b10
devheptagon <dev@heptagonsoft.com>
2021-04-28 17:16:45 +0100

    f

--- project/web/src/pages/business/business.js
+++ project/web/src/pages/business/business.js
@@ -19,31 +19,27 @@ export default function Business() {
   return (
     <div className={styles.outer}>
       <div className={styles.inner}>
-        <h1>BUSINESS</h1>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
-          {data.map((d) => (
-            <div
-              key={d._id}
-              style={{
-                display: "flex",
-                width: "50%",
-              }}
-            >
-              <Link
-                key={d._id}
-                to={`/detail?id=${d._id}&title=${slugify(d.title)}`}
-                as={`/${d._id}?t=${slugify(d.title)}`}
-              >
-                <div>
-                  <h3>{d.title}</h3>
-                  <h5>Sectors: {d.sectors.map((s) => s.title).join(",")}</h5>
-                  <h5>Keywords: {d.tags.map((t) => t.title).join(", ")}</h5>
-                  <h5>Area: {d.area}</h5>
-                  <img alt="list" src={d.cover} style={{ maxWidth: "90%" }} />
-                </div>
-              </Link>
-            </div>
-          ))}
+        <div className={styles.business}>
+          <h1>BUSINESS</h1>
+          <div className={styles.list}>
+            {data.map((d) => (
+              <div key={d._id} className={styles.item}>
+                <Link
+                  key={d._id}
+                  to={`/detail?id=${d._id}&title=${slugify(d.title)}`}
+                  as={`/${d._id}?t=${slugify(d.title)}`}
+                >
+                  <div>
+                    <h3>{d.title}</h3>
+                    <h5>Sectors: {d.sectors.map((s) => s.title).join(",")}</h5>
+                    <h5>Keywords: {d.tags.map((t) => t.title).join(", ")}</h5>
+                    <h5>Area: {d.area}</h5>
+                    <img alt="list" src={d.cover} style={{ maxWidth: "90%" }} />
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
