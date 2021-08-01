fedf2729c28a478dc143b859a0241356dc336c04
devheptagon <dev@heptagonsoft.com>
2021-08-01 16:21:43 +0100

    login

--- project/admin/src/pages/login.js
+++ project/admin/src/pages/login.js
@@ -3,19 +3,120 @@ import { useDispatch } from "react-redux";
 import { setUserAction, setSellersAction } from "redux/app/appActions";
 import { loginEP } from "integration/endpoints/auth";
 import { getSellersEP } from "integration/endpoints/user";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "styles/app.module.scss";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 export default function Login() {
   const history = useHistory();
   const dispatch = useDispatch();
 
-  const login = async () => {
-    const user = await loginEP("test@test.com", "test");
-    dispatch(setUserAction(user));
-    setTimeout(() => {
-      getSellersEP().then((d) => dispatch(setSellersAction(d)));
-      history.push("/home");
-    }, 1000);
-  };
-
-  return <button onClick={login}>login</button>;
+  return (
+    <Container>
+      <Row className={styles.loginWrapper}>
+        <Col xs={12} md={3}>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={yup.object().shape({
+              email: yup.string().required().email(),
+              password: yup.string().required().min(4).max(20),
+            })}
+            onSubmit={async (
+              values,
+              { setSubmitting, setStatus, resetForm }
+            ) => {
+              setSubmitting(true);
+              const user = await loginEP(values.email, values.password);
+              dispatch(setUserAction(user));
+              const sellers = await getSellersEP();
+              dispatch(setSellersAction(sellers));
+              setSubmitting(false);
+              history.push("/home");
+              setStatus({ success: true });
+            }}
+          >
+            {(props) => {
+              const {
+                values,
+                //touched,
+                errors,
+                dirty,
+                isSubmitting,
+                handleChange,
+                handleSubmit,
+              } = props;
+              return (
+                <form onSubmit={handleSubmit}>
+                  <div className={styles.login}>
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>
+                            Email:
+                            <br />
+                            <input
+                              type="text"
+                              onChange={handleChange}
+                              name="email"
+                              id="email"
+                              placeholder="Email"
+                              title="* email"
+                              value={values.email}
+                            />
+                            <span data-id="error">{errors.email}</span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            Password:
+                            <br />
+                            <input
+                              type="password"
+                              onChange={handleChange}
+                              name="password"
+                              id="password"
+                              placeholder="Password"
+                              title="* password"
+                              value={values.password}
+                            />
+                            <span data-id="error">{errors.password}</span>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <button
+                              data-id="save"
+                              type="submit"
+                              disabled={
+                                isSubmitting ||
+                                !dirty ||
+                                Object.keys(errors).length
+                              }
+                            >
+                              Save
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <a>Forgot password</a>
+                            <br />
+                            <a>Register</a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </form>
+              );
+            }}
+          </Formik>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
