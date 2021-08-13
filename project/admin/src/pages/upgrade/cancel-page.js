5822f93ec2f43a58c2579ee52ad3b9be310d3ba9
devheptagon <dev@heptagonsoft.com>
2021-08-13 22:53:18 +0100

    fix

--- project/admin/src/pages/upgrade/cancel-page.js
+++ project/admin/src/pages/upgrade/cancel-page.js
@@ -1,11 +1,12 @@
-import Layout from "Layout";
 import styles from "styles/app.module.scss";
 
 export default function CancelPage() {
   return (
-    <div className={styles.upgrade}>
-      <h3>You cancelled membership!</h3>
-      You can try again any time you want!
+    <div className={styles.upgrade_result}>
+      <div>
+        <h3>You cancelled upgrade!</h3>
+        You can try again any time you want!
+      </div>
     </div>
   );
 }
--- project/admin/src/pages/upgrade/success-page.js
+++ project/admin/src/pages/upgrade/success-page.js
@@ -1,12 +1,14 @@
-import Layout from "Layout";
 import styles from "styles/app.module.scss";
+import { Link } from "react-router-dom";
 
 export default function SuccessPage() {
   return (
-    <div className={styles.upgrade}>
-      <h3>Congratulations!</h3>
-      Your membership has started, please logout and relogin to refresh your
-      settings!
+    <div className={styles.upgrade_result}>
+      <div>
+        <h3>Congratulations!</h3>
+        Your membership has started, please click here to&nbsp;
+        <Link to={`/logout`}>Login</Link> and refresh your settings!
+      </div>
     </div>
   );
 }
